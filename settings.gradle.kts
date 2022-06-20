0cacd694a60e414fbf1c5b823c0c2b833a0ed776
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-06-20 19:40:13 -0300

    fist commit ktor api

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.rodtech.ktor-poc-http-api"
---
+++ src/main/kotlin/com/rodtech/Application.kt
@@ -0,0 +1,12 @@
+package com.rodtech
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.rodtech.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
