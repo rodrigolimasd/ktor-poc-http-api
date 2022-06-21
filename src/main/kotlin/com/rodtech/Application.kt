ce0aa7b3ef7ac27d7ce318224176a0c2d4bf7146
Rodrigo Lima <rodrigolimasd@gmail.com>
2022-06-21 18:53:13 -0300

    feat: implemeted config to database h2

--- src/main/kotlin/com/rodtech/Application.kt
+++ src/main/kotlin/com/rodtech/Application.kt
@@ -1,11 +1,13 @@
 package com.rodtech
 
+import com.rodtech.config.dao.DatabaseFactory
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.rodtech.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        DatabaseFactory.init()
         configureRouting()
         configureSerialization()
     }.start(wait = true)
